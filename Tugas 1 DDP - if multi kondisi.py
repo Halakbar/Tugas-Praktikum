2216ed3ae3493cae3b4bf0956a2fb8e40200408c
Halakbar <92511249+Halakbar@users.noreply.github.com>
2021-11-23 20:51:57 +0800

    Add files via upload

---
+++ Tugas 1 DDP - if multi kondisi.py
@@ -0,0 +1,22 @@
+#Pegawai1
+nama1 = "Ahmad"
+agama1 = "Muslim"
+gaji pokok1 = "4000000"
+jumlah anak1 = "2"
+tunjangan jabatan1 = "0.2"
+tunjangan keluarga1 = "tunjangan keluarga"
+ket1 = "tunjangan keluarga" if nilai >= 10% else "0"
+if(nilai >= 10%): grade1 = "0.1"
+elif(nilai >= 20%): grade1 = "0.2"
+else: grade1 = "Rp. 0"
+gaji kotor1 = ""
+print("Data Pegawai 1:"
+      "\nNama pegawai\t:",nama1,
+      "\nAgama\t\t:",agama1,
+      "\nGaji pokok\t:",gaji pokok1
+      "\nJumlah anak\t:",jumlah anak,
+      "\nTunjangan jabatan\t:",tunjangan jabatan1,
+      "\nTunjangan keluarga\t:",grade1
+      
+      )
+
